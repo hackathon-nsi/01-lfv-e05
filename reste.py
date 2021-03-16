fb30d66296cd0b2090b0b045f8e52f1876c8999d
nsi-lfv-van <79848897+nsi-lfv-van@users.noreply.github.com>
2021-03-16 12:00:33 +0100

    Add files via upload
    reste du programme avec les deux "for"

---
+++ reste.py
@@ -0,0 +1,37 @@
+###-------------------------------------------------------------------------------
+### Name:        module1
+### Purpose:
+###
+### Author:      nico.vand
+###
+### Created:     16/03/2021
+### Copyright:   (c) nico.vand 2021
+### Licence:     <your licence>
+###-------------------------------------------------------------------------------
+##
+
+#
+#                    for y in range(0,448):
+#        for x in range(0,356):
+#
+
+#            pixel = im_3.getpixel((x, y))
+#            p_rouge = pixel[0]
+#            p_vert =  pixel[1]
+#            p_bleu =  pixel[2]
+#            #if change taille de bande
+#            #x change la distance bande-reste (si deplacement à l'arriere de premiere bande x=x-(distance attendue-1) )
+#            #y monte/decent la barre
+#
+#            if 50<x<106:
+#                im_new.putpixel((x+50,y+10),(p_rouge,p_vert,p_bleu))
+#            elif 100<x<156:
+#                im_new.putpixel((x-49,y+5),(p_rouge,p_vert,p_bleu))
+#            elif 150<x<206:
+#                im_new.putpixel((x+50,y+10),(p_rouge,p_vert,p_bleu))
+#            elif 200<x<256:
+#                im_new.putpixel((x-50,y+5),(p_rouge,p_vert,p_bleu))
+#            elif 250<x<306:
+#                im_new.putpixel((x,y+5),(p_rouge,p_vert,p_bleu))
+#            else:
+#                im_new.putpixel((x,y),(p_rouge,p_vert,p_bleu))
