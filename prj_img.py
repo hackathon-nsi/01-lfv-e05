3a4289814a6d5447d34bfe1aec8ce0216f385bd5
nsi-lfv-ray <79856491+nsi-lfv-ray@users.noreply.github.com>
2021-04-25 11:12:28 +0200

    Update prj_img.py

--- prj_img.py
+++ prj_img.py
@@ -19,7 +19,9 @@ import numpy as np
 img = Image.open("pictures/Capture_hackathon.PNG").convert('RGB')
 img_arr = np.array(img)
 
-#Cette fonction permet de couper des triangles dans les 4 cotes
+# la fonction "triangle" permet de couper des triangles dans les 4 cotes
+
+def triangles():
 
 img_arr[0 : 50, 0 : 50] = (255, 255, 255)
 img_arr[315 : 50, 315 : 50] = (255, 255, 255)
@@ -27,11 +29,18 @@ img_arr[315 : 392, 315 : 392] = (255, 255, 255)
 img_arr[50 : 392, 50 : 392] = (255, 255, 255)
 img = Image.fromarray(img_arr)
 
-#sdgösldmgspkdngsdg
+#La fonction "sombre" permet de changer les couleur pls sombre
 
-for x in range(img.size[0]):
-    for y in range (pic.size[1]):
-        r, g , b =omg.getpixel((x,y))
-        img.putpixel((x,y),(r//2 , g//2 , b//2))
+def sombre():
 
+   im_data = im.load()
+height,width = im.size
+for loop1 in range(height):
+    for loop2 in range(width):
+        r,g,b = im_data[loop1,loop2]
+        im_data[loop1,loop2] = r//2,g//2,b//2
+im_new.save('changed.jpeg')
+
+triangles()
+sombre()
 img.show()
